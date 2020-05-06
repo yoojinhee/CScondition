c9eba8678b14a6b201fb31e2574b9378df6b9b06
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-05-06 15:00:57 +0900

    # 13-5. switch 조건문 실습 예제

--- CScondition/Program.cs
+++ CScondition/Program.cs
@@ -13,12 +13,21 @@ namespace CScondition
             Console.Write("숫자 입력 :");
             var input = int.Parse(Console.ReadLine());
 
-            if (input % 2 == 0)
+            /*if (input % 2 == 0)
             {
                 Console.WriteLine("짝수");
             }else if (input % 2 == 1)
             {
                 Console.WriteLine("홀수");
+            }*/
+            switch (input % 2)
+            {
+                case 0:
+                    Console.WriteLine("짝수");
+                    break;
+                case 1:
+                    Console.WriteLine("홀수");
+                    break;
             }
             //현재 시간 구하기
             Console.WriteLine(DateTime.Now.Year);
@@ -40,6 +49,19 @@ namespace CScondition
             else
             {
                 Console.WriteLine("저녁 먹을 시간입니다");
+            }
+            Console.Write("이번달은 몇 월 인가요:");
+            int input2 = int.Parse(Console.ReadLine());
+            switch (input2)
+            {
+                case 12:case 1:case 2:
+                    Console.WriteLine("겨울"); break;
+                case 3: case 4: case 5:
+                    Console.WriteLine("봄"); break;
+                case 6: case 7: case 8:
+                    Console.WriteLine("여름"); break;
+                case 9:case 10: case 11:
+                    Console.WriteLine("봄"); break;
             }
         }
     }

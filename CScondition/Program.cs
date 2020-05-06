1d13590a0f21efc9a49089e77763a4078ee1f531
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-05-06 14:52:17 +0900

    #2 13-4. if else if 조건문

--- CScondition/Program.cs
+++ CScondition/Program.cs
@@ -33,18 +33,14 @@ namespace CScondition
             {
             Console.WriteLine("아침 먹을 시간입니다");
             }
+            else if(DateTime.Now.Hour<15)
+            {
+                Console.WriteLine("점심 먹을 시간입니다");
+            }
             else
             {
-                if(DateTime.Now.Hour<15)
-                {
-                    Console.WriteLine("점심 먹을 시간입니다");
-                }
-                else
-                {
-                    Console.WriteLine("저녁 먹을 시간입니다");
-                }
+                Console.WriteLine("저녁 먹을 시간입니다");
             }
-           
         }
     }
 }

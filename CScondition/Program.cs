b995209e83d8191d8abff4d074a7d23b50882e30
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-05-06 14:37:12 +0900

    오전 오후 구하기

--- CScondition/Program.cs
+++ CScondition/Program.cs
@@ -19,8 +19,25 @@ namespace CScondition
             }else if (input % 2 == 1)
             {
                 Console.WriteLine("홀수");
-
             }
+            //현재 시간 구하기
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+            //오전 오후 구분
+            if (DateTime.Now.Hour < 12)
+            {
+            Console.WriteLine("오전입니다");
+            }
+            else
+            {
+                Console.WriteLine("오후입니다");
+            }
+           
         }
     }
 }

4602c6e419a6b39eeec1435150bac28fc07ee3ae
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-05-06 14:31:06 +0900

    if 조건문 실습

--- CScondition/Program.cs
+++ CScondition/Program.cs
@@ -10,6 +10,17 @@ namespace CScondition
     {
         static void Main(string[] args)
         {
+            Console.Write("숫자 입력 :");
+            var input = int.Parse(Console.ReadLine());
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("짝수");
+            }else if (input % 2 == 1)
+            {
+                Console.WriteLine("홀수");
+
+            }
         }
     }
 }
